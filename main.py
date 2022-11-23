90b06a6ee3f522d4aa7a31d4f115c7edde094edd
swastik79 <mishraswastik79@gmail.com>
2022-11-23 15:27:39 +0800

    This is third version.

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi("Group A!")
+    print_hi("Group B!")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
